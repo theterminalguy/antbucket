12c7ff8f4d5ab3893822d89e456a0c01ae3ffb03
andela-sdamian <simon.peter@andela.com>
2016-05-02 20:15:19 +0100

    docs(api): api documentation for various end points
    write documentation for all endpoints in the application as well as
    badge repository for continous integration badges
    
    [Finishes #118776047]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do 
+  get '/', :to => redirect('/api/docs/index.html')
+  get '/tester', :to => redirect('/api/tester/index.html')
   namespace :api do 
     namespace :v1 do 
       resources :users, only: :create
