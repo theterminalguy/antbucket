c870752384686f80cfc98843683bae3eaa6b0fbd
andela-sdamian <simon.peter@andela.com>
2016-05-02 10:56:29 +0100

    test(user): test for user controllers
    test for users controllers to verify if users can send POST request and
    get data returned as json

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+module Api 
+  module V1 
+    class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      respond_to :json 
+
+      def create 
+        @user = User.new(user_params) 
+
+        if @user.save
+          render json: @user, status: 201
+        else
+          render json: { errors: @user.errors }, status: 422
+        end
+      end
+
+
+      def user_params
+        params.permit(:email, :password) 
+      end 
+    end
+  end 
+end 
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,17 @@
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+Dir[::Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
-  
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.raise_errors_for_deprecations!
+  expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
+  config.include Request::JsonHelpers, type: :controller
 end
